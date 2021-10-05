1bcd929204b7b198f89541d0a9087a271f050ba4
Ciaran Mc Cusker <cmccusker@aflac.com>
2021-10-05 15:20:09 +0100

    first commit to add angular project

---
+++ mobileapp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ClaimComponent } from './claim/claim.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    ClaimComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
